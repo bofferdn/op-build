ce30d602aaf83f110113a6053b30625cbd1f9278
Nick Bofferding <bofferdn@us.ibm.com>
2016-10-08 12:51:19 -0500

    SECUREBOOT: Point op-build at non-padded, non-ECCd SBE, SBEC, WINK p8 images

--- openpower/package/hostboot-binaries/hostboot_binaries.mk
+++ openpower/package/hostboot-binaries/hostboot_binaries.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-HOSTBOOT_BINARIES_VERSION ?= 61c5c1a6318cad6d20b0926403e4444defb9a60a
-HOSTBOOT_BINARIES_SITE ?= $(call github,open-power,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
+HOSTBOOT_BINARIES_VERSION ?= 8844c618fe29fe6dd5ccb314146ec3a440a3b57a
+HOSTBOOT_BINARIES_SITE ?= $(call github,bofferdn,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
 HOSTBOOT_BINARIES_LICENSE = Apache-2.0
 
 HOSTBOOT_BINARIES_INSTALL_IMAGES = YES
